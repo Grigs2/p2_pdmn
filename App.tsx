54454934e829705414d96cf7acc74a375f065ed8
Otavio Grigolon <otaviogrigolon@gmail.com>
2026-05-27 07:35:42 -0300

    feat: Conexão com a api para coleta de dados de paises

--- App.tsx
+++ App.tsx
@@ -1,13 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable
+} from 'react-native';
+import restcountriesClient from './utils/restcountriesClient';
+
+
 
 export default function App() {
+
+  const [carregando, setCarregando] = useState(false);
+
+
+
+
+  const [nomePais, setNomePais] = useState<string>('');
+
+  const buscarTodos = () => {
+  setCarregando(true);
+    restcountriesClient.get('all', {
+      params: {
+        fields: 'name,capital'
+      }
+    })
+  .then(result => {
+    console.log(result.data); 
+  })
+  .catch(e => {
+    console.log('Erro:', e);
+  })
+  .finally(() => {
+    setCarregando(false);
+  });
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Pressable
+      style={styles.button}
+      onPress={buscarTodos}>
+      <text
+      style={styles.buttonText}>Pesquisar todos</text>
+      </Pressable>
+
+
+        <TextInput
+        style={styles.estilo1}
+        placeholder='Digite Algo...'
+        onChangeText={setNomePais}
+        value={nomePais}
+        />
+
+      <Pressable
+      style={styles.button}>
+      <text
+      style={styles.buttonText}>Pesquisar nome</text>
+      </Pressable>
+    
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +77,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  estilo1:{
+    width: '80%',
+    borderColor:'gray',
+    borderWidth: 1,
+    marginBottom:12,
+    padding:8,
+    textAlign: 'center',
+    borderRadius: 4
+  },
+
+  button:{
+    borderColor:'gray',
+    backgroundColor:'pink',
+    borderWidth: 1,
+    marginBottom:12,
+    padding:8,
+    textAlign: 'center',
+    borderRadius: 18
+  },
+
+  buttonText:{
+    fontFamily: 'arial',
+    textAlign: 'center'
+  }
 });
